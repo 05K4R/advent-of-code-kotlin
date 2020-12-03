25cef6cdf1ed7c1191053544c50c85910d59aa2b
Oskar Kärrman <oskar.karrman@tacdis.com>
2020-12-03 10:28:09 +0100

    Clean up input reader

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,34 @@
 import java.io.File
 
 fun main() {
-    val puzzle = loadPuzzle(2020, 3)
-    println(puzzle.answerPart2())
+    val year = 2020; val day = 3; val part = 2
+    println("Day $day part $part = ${calculateAnswer(year, day, part)}")
 }
 
 class InputReader(year: Int, day: Int) {
     private val source = "src/main/resources/input/year$year/day${day}.txt"
+    private fun <T> read(block: (Sequence<String>) -> T) = File(source).useLines { block.invoke(it) }
 
-    fun asIntSet() = File(source).useLines { lines -> lines.map { it.toInt() }.toSet() }
-    fun asStringSet() = File(source).useLines { it.toSet() }
-    fun asStringList() = File(source).useLines { it.toList() }
+    fun asIntSet() = read { lines -> lines.map { it.toInt() }.toSet() }
+    fun asStringSet() = read { it.toSet() }
+    fun asStringList() = read { it.toList() }
 }
 
+class InvalidPartException(part: Int) : RuntimeException("Invalid part: $part")
+class NoSolutionFoundException : RuntimeException()
 interface Puzzle {
     fun answerPart1(): Int
     fun answerPart2(): Int
 }
 
-class NoSolutionFoundException : RuntimeException()
-
-fun loadPuzzle(year: Int, day: Int): Puzzle {
-    return Class.forName("year$year.Day$day")
+private fun calculateAnswer(year: Int, day: Int, part: Int): Int {
+    val puzzle = Class.forName("year$year.Day$day")
         ?.getDeclaredConstructor(InputReader::class.java)
         ?.newInstance(InputReader(year, day))
         as Puzzle
+    return when(part) {
+        1 -> puzzle.answerPart1()
+        2 -> puzzle.answerPart2()
+        else -> throw InvalidPartException(part)
+    }
 }
